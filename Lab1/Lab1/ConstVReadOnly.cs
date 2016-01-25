a739f1d83ec59f0d1d7f2715e8d9407198c91d08
Daniel Vang <danielvang@ghalliance.org>
2016-01-25 11:50:10 -0600

    Initial commit to lab1

---
+++ Lab1/Lab1/ConstVReadOnly.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class ConstVReadOnly
+    {
+        public const double PI = 3.1;
+        public ConstVReadOnly()
+        {
+            
+        PI = 3.1415;
+    }
+    }
+}
