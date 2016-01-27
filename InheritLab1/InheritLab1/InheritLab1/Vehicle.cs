3a629348f3d353932823595057fe7dc2c3b92756
Daniel Vang <danielvang@ghalliance.org>
2016-01-27 15:00:13 -0600

    created vehicle classes

---
+++ InheritLab1/InheritLab1/InheritLab1/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritLab1
+{
+    public abstract class Vehicle
+    {
+        int mpg;
+        int horsePower;
+        bool transmission;
+        
+        public void trans()
+        {
+            if(transmission == true)
+            {
+                Console.WriteLine("Automatic Transmission");
+            }
+            else if(transmission == false)
+            {
+                Console.WriteLine("Manual Transmission");
+            }
+        }
+    }
+}
